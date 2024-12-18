ff93fedaa074bcdf4eba2baba9180bf18a5b0341
Jeffrey Durieux <jeffreydurieux@users.noreply.github.com>
2024-12-18 08:50:13 +0100

    Easter egg
    Added an easter egg to the app. On Spinoza's birthday, his image is shown.

--- Sterrenbeeld.R
+++ Sterrenbeeld.R
@@ -4,19 +4,35 @@ library(shiny)
 get_zodiac_sign <- function(date) {
   day <- as.integer(format(date, "%d"))
   month <- as.integer(format(date, "%m"))
+  year <- as.integer(format(date, "%Y"))
   
-  if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) return("aries")
-  if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) return("taurus")
-  if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) return("gemini")
-  if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) return("cancer")
-  if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) return("leo")
-  if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) return("virgo")
-  if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) return("libra")
-  if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) return("scorpio")
-  if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) return("sagittarius")
-  if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) return("capricorn")
-  if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) return("aquarius")
-  if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) return("pisces")
+  if (year == 1632 && day == 24 && month == 11) {
+    return("spinoza")
+  } else if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+    return("aries")
+  } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+    return("taurus")
+  } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+    return("gemini")
+  } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+    return("cancer")
+  } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+    return("leo")
+  } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+    return("virgo")
+  } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+    return("libra")
+  } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+    return("scorpio")
+  } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+    return("sagittarius")
+  } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+    return("capricorn")
+  } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+    return("aquarius")
+  } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+    return("pisces")
+  }
 }
 
 # UI
